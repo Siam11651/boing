1e5ad5c2caae4b4e7c40d200ce29b365eb596e55
Siam11561 <siam11651@outlook.com>
2024-06-14 17:23:10 +0600

    kaaj kori

--- scripts/PaddleNormal.cs
+++ scripts/PaddleNormal.cs
@@ -9,18 +9,32 @@ public partial class PaddleNormal : Node2D
 		UNSTUCK, INIT, STUCK
 	}
 
+	[Export]
+	private float mBallInitSpeed;
 	private BallStuckState mBallStuckState = BallStuckState.INIT;
 	private int mWindowWidth;
 	private double mDeltaTime = 0.0;
 	private AnimatedSprite2D mAnimatedSprite2D;
 	private Node2D mBallInit;
-	private Node2D mBall;
+	private RigidBody2D mBall;
+
+	public void ReleaseBall()
+	{
+		if(mBallStuckState != BallStuckState.UNSTUCK)
+		{
+			mBallStuckState = BallStuckState.UNSTUCK;
+			float velocityComponentY = -0.5f - (float)new Random().NextDouble() / 2.0f;
+			float velocityComponentX = MathF.Sqrt(1.0f - velocityComponentY * velocityComponentY);
+			mBall.Freeze = false;
+			mBall.LinearVelocity = new Vector2(velocityComponentX, velocityComponentY) * mBallInitSpeed;
+		}
+	}
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		mWindowWidth = GetWindow().Size.X;
-		mBall = GetParent().GetNode<Node2D>("Ball");
+		mBall = GetParent().GetNode<RigidBody2D>("Ball");
 		mBallInit = GetNode<Node2D>("BallInit");
 
 		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("default");
@@ -41,4 +55,17 @@ public partial class PaddleNormal : Node2D
 
 		}
 	}
+
+    public override void _Input(InputEvent @event)
+    {
+        if(@event.GetType() == typeof(InputEventMouseButton))
+		{
+			InputEventMouseButton mouseButtonEvent = (InputEventMouseButton)@event;
+
+			if(!mouseButtonEvent.Pressed && mouseButtonEvent.ButtonIndex == MouseButton.Left)
+			{
+				ReleaseBall();
+			}
+		}
+    }
 }
